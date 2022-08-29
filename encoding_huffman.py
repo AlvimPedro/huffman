e5d53e655c4a3362f1d08e68468cc78608ad98ae
AlvimPedro <pedroalvim2000@hotmail.com>
2022-08-28 21:55:12 -0300

    Lendo do arquivo e comprimindo, sem header

--- encoding_huffman.py
+++ encoding_huffman.py
@@ -5,6 +5,9 @@
 # byte = 'ABBBBBBBBBBBBBBBBBBBBBBBBBBCCAAAAADDCCCCCCCCCCCCCGGGHHHHUZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
 
 # Criação dos nós
+from ast import While
+
+
 class NodeTree(object):
 
     def __init__(self, left=None, right=None):
@@ -32,40 +35,31 @@ def huffman_code_tree(node, left=True, binString=''):
     return d
 
 
-# Cálculo da frequência de repetição dos caracteres 
-# freq = {}
-# for c in byte:
-#     if c in freq:
-#         freq[c] += 1
-#     else:
-#         freq[c] = 1
-
-# freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-# print(freq)
-
 f = open('a.txt', 'rb')
-
 byte = f.read(1)
 
 contBytes = [0] * 256 
 contTotal = 0
 
+#Conta a aparição de cada byte do arquivo
 while byte:
     contBytes[int.from_bytes(byte, 'big')] += 1
     contTotal += 1
 
     byte = f.read(1)
 
-contBytesMapped = [(0,0)] * 256
-for i in range(256):
-    contBytesMapped[i] = (i, contBytes[i])
 
+#É feito um mapeamento de cada simbolo com a frequência e ordenado
+contBytesMapped = []
+for i in range(256):
+    if contBytes[i] > 0:
+        contBytesMapped.append((str(i), contBytes[i]))
 
 def takeSecond(elem):
     return elem[1]
 contBytesMapped.sort(reverse=True, key=takeSecond)
 
-print(contBytesMapped)
+# print(contBytesMapped)
 
 
 freq = contBytesMapped
@@ -81,9 +75,47 @@ while len(nodes) > 1:
 
     nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
 
+
 huffmanCode = huffman_code_tree(nodes[0][0])
+
 
 print(' Chars | Huffman Tree ')
 print('----------------------')
 for (char, frequency) in freq:
     print(' %-4r |%12s' % (char, huffmanCode[char]))
+
+#Criação do arquivo comprimido
+compressedFile = open('compressed.pphuff', 'wb')
+compressedBytes = []
+
+f = open('a.txt', 'rb')
+byte = f.read(1)
+
+#Rodar o arquivo novamente com o dicionário de Huffman e assim codificar cada byte
+nextBinByte = '' #Próximo byte binário a ser escrito no arquivo
+while byte:
+    readByte = str(int.from_bytes(byte, 'big'))
+    codedBin = huffmanCode[readByte]
+
+    while len(codedBin) > 0:
+        if len(nextBinByte) < 8:
+            nextBinByte += codedBin[:1]     #Primeio bit
+            codedBin = codedBin[1:]
+        else:
+            compressedBytes.append(int(nextBinByte, base=2))
+            nextBinByte = ''
+    
+    byte = f.read(1)
+
+#Caso falte bits no último byte, preencher até ser um byte
+#No Header do arquivo comprimido vai ter essa quantidade de bits que deve ser descartado do final
+contBitsFinal = 0
+while len(nextBinByte) < 8:
+    contBitsFinal += 1
+    nextBinByte += '1'
+
+compressedBytes.append(int(nextBinByte, base=2))
+
+print(compressedBytes)
+compressedBytesArray = bytearray(compressedBytes)
+compressedFile.write(compressedBytesArray)
